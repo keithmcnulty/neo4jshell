013e8c8e1a8103c8cbd195d54cadea2fbe36347d
Keith McNulty <keith_mcnulty@mckinsey.com>
2019-07-23 15:23:03 +0100

    Added neo4j_restart()

---
+++ R/neo4j_restart.R
@@ -0,0 +1,9 @@
+#' Restart a local Neo4J database
+#'
+#' @param neo4j_path Path to the Neo4J executable (usually in the bin directory of the Neo4J installation)
+#'
+#' @return System messages
+
+neo4j_restart <- function(neo4j_path = "neo4j") {
+  sys::exec_background(neo4j_path, "restart")
+}
